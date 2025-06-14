cd4e0293969e41ca2b3d3581977f41fa8aff2952
Lenise Moraes <lemoraes.santana@gmail.com>
2025-06-14 15:03:12 -0300

    Saving the html changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
       </header>
+      <h1>Paris</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img src="#" alt="" /> 6ºC
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Preciptation 15%</li>
+            <li>Huminity 72%</li>
+            <li>Wind 13km/h</li>
+          </ul>
+        </div>
+      </div>
+      <footer>
+        This project was coded by
+        <a href="https://www.linkedin.com/in/lenise-m-santana-72b824311/">
+          Lenise Moraes
+        </a>{" "}
+        and is open source in Github.
+      </footer>
     </div>
   );
 }
